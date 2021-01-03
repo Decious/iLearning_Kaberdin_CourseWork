98b94567f66d5ebd84fb41f207c998057876c320
Decious <Andrey@Kaberdin.com>
2021-01-03 22:05:02 +0300

    INIT-13 Profile. Added avatar picture for the user and page description.

--- KaberdinCourseiLearning/KaberdinCourseiLearning/Areas/User/Pages/Profile.cshtml.cs
+++ KaberdinCourseiLearning/KaberdinCourseiLearning/Areas/User/Pages/Profile.cshtml.cs
@@ -1,7 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
+using KaberdinCourseiLearning.Helpers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +12,57 @@ namespace KaberdinCourseiLearning.Areas.User.Pages
 {
     public class ProfileModel : PageModel
     {
-        public void OnGet()
+        private UserManager<IdentityUser> userManager;
+        private IWebHostEnvironment webHostEnvironment;
+        public string AvatarPath { get; set; }
+        public bool PermittedToChange { get; set; }
+        public ProfileModel(UserManager<IdentityUser> userManager, IWebHostEnvironment webHostEnvironment)
         {
+            this.userManager = userManager;
+            this.webHostEnvironment = webHostEnvironment;
+        }
+        public IdentityUser PageUser { get; set; }
+        public async Task<IActionResult> OnGetAsync(string name)
+        {
+            if(name != null)
+            {
+                var user = await userManager.FindByNameAsync(name);
+                if (user != null)
+                {
+                    PageUser = user;
+                    AvatarPath = Path.Combine(webHostEnvironment.WebRootPath, "images", "User", "Avatar", PageUser.Id + ".png");
+                    var guest = await userManager.GetUserAsync(User);
+                    PermittedToChange = await new UserValidator(userManager).isUserOwnerOrAdminAsync(guest, name);
+                    return Page();
+                }
+            }
+            return Redirect("~/Index");
+        }
+        public async Task<IActionResult> OnPostUploadAvatar(IFormFile file,string name)
+        {
+            var validator = new UserValidator(userManager);
+            var user = await userManager.GetUserAsync(User);
+            PageUser = await userManager.FindByNameAsync(name);
+            if(await validator.isUserOwnerOrAdminAsync(user,name))
+            {
+                try
+                {
+                    if (file.Length > 0)
+                    {
+                        AvatarPath = Path.Combine(webHostEnvironment.WebRootPath, "images", "User", "Avatar", PageUser.Id + ".png");
+                        using (var stream = System.IO.File.Create(AvatarPath))
+                        {
+                            await file.CopyToAsync(stream);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"[AVATAR {user.UserName}]Exception during file upload.\n {e.Message}");
+                }
+                return new OkResult();
+            }
+            return Forbid();
         }
     }
 }
---
+++ KaberdinCourseiLearning/KaberdinCourseiLearning/wwwroot/js/Profile.js
@@ -0,0 +1,87 @@
+﻿if (isPermitted()) {
+    var buttonEdit = $("#DescChange");
+    var buttonAccept = $("#DescAccept");
+    var description = $("#DescriptionContent");
+    var invoker = $("#ProfileDescription");
+    init();
+}
+function isPermitted() {
+    return ($("#AvatarChange").length > 0);
+}
+function init() {
+    prepareAvatar();
+    prepareDescription();
+    prepareDropzone();
+}
+function prepareAvatar() {
+    prepareMouseEvents($("#ProfilePicture"), $("#AvatarChange"));
+}
+function prepareMouseEvents(invokerElement, subject) {
+    invokerElement.mouseenter(function () {
+        subject.show(0.5);
+    })
+    invokerElement.mouseleave(function () {
+    subject.hide(0.5);
+    })
+}
+function prepareDescription() {
+    prepareMouseEvents(invoker, buttonEdit);
+    prepareDescOnEdit();
+    prepareEditAccept();
+}
+function prepareDescOnEdit() {
+    buttonEdit.on('click', function () {
+        setDescAsTextArea();
+        unBindMouseEvents(invoker);
+        buttonAccept.show();
+        buttonEdit.hide();
+    })
+}
+function setDescAsTextArea() {
+    let text = description.children("p").text();
+    description.html("<textarea style='width: 100%; height: 100%; background: transparent;border: none;' >" + text + "</textarea>");
+    var textarea = description.children("textarea");
+    textarea.focus();
+    textarea.on('keypress', function (e) {
+        if (e.which == 13) {
+    acceptDescription();
+        }
+    });
+}
+function unBindMouseEvents(element) {
+    element.unbind("mouseenter");
+    element.unbind("mouseleave");
+}
+
+function prepareEditAccept() {
+    buttonAccept.on('click', function () {
+        let newText = description.children("textarea").val();
+        setDescText(newText);
+        prepareMouseEvents(invoker, buttonEdit);
+        buttonAccept.hide();
+    })
+}
+function acceptDescription() {
+    let newText = description.children("textarea").val();
+    setDescText(newText);
+    prepareMouseEvents(invoker, buttonEdit);
+    buttonAccept.hide();
+}
+function setDescText(newText) {
+    description.html("<p style='max-height: 80px' class='text-break text-wrap text-truncate' >" + newText + "</p>");
+}
+function prepareDropzone() {
+    Dropzone.options.AvatarDropzone = {
+        maxFilesize: 4,
+        maxFiles: 1,
+        acceptedFiles: "image/*",
+        resizeWidth: 205,
+        resizeHeight: 205,
+        init: function () {
+            this.on("success", function (event) { setTimeout(location.reload(), 2000); });
+            this.on("sending", function (file, xhr, formData) {
+                formData.append("name", $("#UserName").text());
+            });
+        }
+    }
+}
