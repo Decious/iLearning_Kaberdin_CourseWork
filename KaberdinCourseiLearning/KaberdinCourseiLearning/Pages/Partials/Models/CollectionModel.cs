3ab3b7094f812a7eeeb373a404ff4504ab2aa5fd
Decious <Andrey@Kaberdin.com>
2021-01-08 18:34:20 +0300

    EDIT-29 Collection. Moved collection html view into partial view for better reusability.

---
+++ KaberdinCourseiLearning/KaberdinCourseiLearning/Pages/Partials/Models/CollectionModel.cs
@@ -0,0 +1,11 @@
+﻿using KaberdinCourseiLearning.Data.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KaberdinCourseiLearning.Pages.Partials.Models
+{
+    public class CollectionModel : PageModel
+    {
+        public ProductCollection Collection { get; set; }
+        public bool PermittedToChange { get; set; }
+    }
+}
