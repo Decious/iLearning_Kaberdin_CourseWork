4150b3ff016551eb613f053a504245651b3ca620
Decious <Andrey@Kaberdin.com>
2021-01-11 17:09:24 +0300

    EDIT-47 ServerResponse. Added url property to server response for easier redirection.

--- KaberdinCourseiLearning/KaberdinCourseiLearning/Data/ServerResponse.cs
+++ KaberdinCourseiLearning/KaberdinCourseiLearning/Data/ServerResponse.cs
@@ -7,14 +7,16 @@ namespace KaberdinCourseiLearning.Data
 {
     public class ServerResponse
     {
-        public ServerResponse(bool successful, string message)
+        public ServerResponse(bool successful, string message,string url=null)
         {
-            this.successful = successful;
-            this.message = message;
+            Successful = successful;
+            Message = message;
+            Url = url;
         }
         public static ServerResponse MakeForbidden() => new ServerResponse(false, "You are not authorized to commit these changes.");
         public static ServerResponse MakeSuccess() => new ServerResponse(true, "Action completed successfully.");
-        public bool successful { get; set; }
-        public string message { get; set; }
+        public bool Successful { get; set; }
+        public string Message { get; set; }
+        public string Url { get; set; }
     }
 }
