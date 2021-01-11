12de006ec6c338e91109c7a7c79dfc352bedc96e
Decious <Andrey@Kaberdin.com>
2021-01-11 19:41:27 +0300

    EDIT-49 Managers. Added tag manager.

---
+++ KaberdinCourseiLearning/KaberdinCourseiLearning/Managers/TagManager.cs
@@ -0,0 +1,41 @@
+﻿using KaberdinCourseiLearning.Data;
+using KaberdinCourseiLearning.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KaberdinCourseiLearning.Managers
+{
+    public class TagManager
+    {
+        private ApplicationDbContext context;
+        public TagManager(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public Tag[] GetAllTags()=>context.Tags.ToArray();
+        public Tag GetTag(int tagID) => context.Tags.Find(tagID);
+        public Tag GetTag(string tagValue) => context.Tags.FirstOrDefault(e => e.TagValue == tagValue);
+        public Product[] GetProductsWithTag(Tag tag)
+        {
+            context.Entry(tag).Collection(e => e.ProductTags);
+            var products = new Product[tag.ProductTags.Count];
+            foreach(var prodTag in tag.ProductTags)
+            {
+                products.Append(context.Products.Find(prodTag.ProductID));
+            }
+            return products.ToArray();
+        }
+        public async Task AddTagAsync(Tag tag)
+        {
+            await context.Tags.AddAsync(tag);
+            await context.SaveChangesAsync();
+        }
+        public async Task UpdateTagAsync(Tag tag)
+        {
+            context.Tags.Update(tag);
+            await context.SaveChangesAsync();
+        }
+    }
+}
--- KaberdinCourseiLearning/KaberdinCourseiLearning/Startup.cs
+++ KaberdinCourseiLearning/KaberdinCourseiLearning/Startup.cs
@@ -62,6 +62,7 @@ namespace KaberdinCourseiLearning
             services.AddScoped<CollectionManager>();
             services.AddScoped<ProfileManager>();
             services.AddScoped<ProductManager>();
+            services.AddScoped<TagManager>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
