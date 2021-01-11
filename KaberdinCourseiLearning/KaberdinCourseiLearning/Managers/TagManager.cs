7c0b33f3a01dc32147027a1521c1f40ca4a830d9
Decious <Andrey@Kaberdin.com>
2021-01-11 22:16:10 +0300

    EDIT-51 Collection. Index page now shows tags of each item in the collection.

--- KaberdinCourseiLearning/KaberdinCourseiLearning/Managers/TagManager.cs
+++ KaberdinCourseiLearning/KaberdinCourseiLearning/Managers/TagManager.cs
@@ -1,5 +1,6 @@
 ﻿using KaberdinCourseiLearning.Data;
 using KaberdinCourseiLearning.Data.Models;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -53,6 +54,18 @@ namespace KaberdinCourseiLearning.Managers
                 var newPrTag = new ProductTag() { ProductID = productID, TagID = found.TagID };
                 await AddProductTagAsync(newPrTag);
             }
+        }
+        public async Task<string> GetProductTags(Product prod)
+        {
+            await context.Entry(prod).Collection(c => c.Tags).LoadAsync();
+            var prodtags = prod.Tags.ToArray();
+            var tags = new List<string>();
+            foreach(var tag in prodtags)
+            {
+                await context.Entry(tag).Reference(t => t.Tag).LoadAsync();
+                tags.Add(tag.Tag.TagValue);
+            }
+            return string.Join(",", tags);
         }
     }
 }
---
+++ KaberdinCourseiLearning/KaberdinCourseiLearning/wwwroot/js/Collection/Index.js
@@ -0,0 +1,17 @@
+﻿var values = $("[name='columnValue']");
+values.each(function (i,e) {
+    let value = $(e).text();
+    if (value == "on") $(e).text('Yes');
+    if (value == "off") $(e).text('No');
+})
+var tagElements = $("[name='tags']");
+tagElements.each(function (i, element) {
+    let e = $(element);
+    let tags = e.text().split(",");
+    let css = "class='badge badge-dark badge-pill'";
+    var html = [];
+    tags.forEach(function (v, i, arr) {
+        html.push("<span "+css+">"+v+"</span>");
+    })
+    e.html(html);
+})
