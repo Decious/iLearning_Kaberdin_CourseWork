306d6bfb36efffc8781ce0bf26e51895311e15d5
Decious <Andrey@Kaberdin.com>
2021-01-13 20:08:37 +0300

    EDIT-61 SignalR. Added data model to pass through to client.

---
+++ KaberdinCourseiLearning/KaberdinCourseiLearning/Data/CommentResponse.cs
@@ -0,0 +1,25 @@
+﻿using KaberdinCourseiLearning.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KaberdinCourseiLearning.Data
+{
+    public class CommentResponse
+    {
+        public string Message { get; set; }
+        public string[] Roles { get; set; }
+        public string UserName { get; set; }
+        public string AvatarUrl { get; set; }
+        public string CreationDate { get; set; }
+        public CommentResponse(CustomUser user,Comment comment,string[] roles)
+        {
+            Message = comment.Message;
+            Roles = roles;
+            UserName = user.UserName;
+            AvatarUrl = user.AvatarUrl;
+            CreationDate = comment.CreationTime.ToShortDateString() +" "+ comment.CreationTime.ToShortTimeString();
+        }
+    }
+}
