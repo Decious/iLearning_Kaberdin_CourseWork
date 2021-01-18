338f342e730dd1a0c9d55b1756c1190ddbf0db18
Decious <Andrey@Kaberdin.com>
2021-01-18 19:51:01 +0300

    EDIT-92 Main page. Added latest items and biggest collections onto the main page.

--- KaberdinCourseiLearning/KaberdinCourseiLearning/Pages/Index.cshtml.cs
+++ KaberdinCourseiLearning/KaberdinCourseiLearning/Pages/Index.cshtml.cs
@@ -2,24 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using KaberdinCourseiLearning.Data;
+using KaberdinCourseiLearning.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace KaberdinCourseiLearning.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private ApplicationDbContext context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ApplicationDbContext context)
         {
-            _logger = logger;
+            this.context = context;
         }
-
+        public IEnumerable<Tag> Tags { get; set; }
+        public IEnumerable<Product> LastItems { get; set; }
+        public IEnumerable<ProductCollection> BiggestCollections { get; set; }
         public void OnGet()
         {
-
+            Tags = context.Tags.ToArray();
+            LastItems = context.Products.Include(p=>p.Collection).Take(10).OrderByDescending(p => p.CreationDate).ToArray();
+            BiggestCollections = context.ProductCollections.Take(10).OrderByDescending(c => c.Products.Count).ToArray();
         }
     }
 }
