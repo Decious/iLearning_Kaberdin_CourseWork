f49148afb8e56d1aef47b4091ecb1c465ba769ed
Decious <Andrey@Kaberdin.com>
2021-01-20 23:34:09 +0300

    EDIT-99 Theme. Changed primary colors and fixed searchbar. Added theme switch.

---
+++ KaberdinCourseiLearning/KaberdinCourseiLearning/wwwroot/js/ThemeLoader.js
@@ -0,0 +1,30 @@
+﻿if (localStorage.getItem('theme') === 'theme-dark') {
+    setTheme('theme-dark', 'theme-light');
+} else {
+    setTheme('theme-light', 'theme-dark');
+}
+function setTheme(themeName, oldTheme) {
+    document.getElementById(oldTheme).setAttribute("disabled", "true");
+    localStorage.setItem('theme', themeName);
+    document.getElementById(themeName).removeAttribute("disabled");
+}
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-light', 'theme-dark');
+    } else {
+        setTheme('theme-dark', 'theme-light');
+    }
+}
+
+window.addEventListener("load", function (event) {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setChecked(false);
+    } else {
+        setChecked(true);
+    }
+});
+function setChecked(value) {
+    if (value) {
+        document.getElementById("themeBtn").setAttribute('checked', true);
+    }
+}
